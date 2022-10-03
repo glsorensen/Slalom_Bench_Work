2a50796446ff4a5d096d157f0595d132b8e8412f
Gunnar Sorensen <gunnar.sorensen@slalom.com>
2022-10-03 10:25:27 -0600

    addUserMarker method

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,6 +1,7 @@
 /// <reference types="@types/google.maps" />
-// import { User } from './User';
+import { User } from './User';
 // import { Company } from './Company';
+import { CustomMap } from './CustomMap'
 
 // const user = new User();
 // const company = new Company();
@@ -8,10 +9,7 @@
 // console.log(user);
 // console.log(company);
 
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+const user = new User();
+const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user)
