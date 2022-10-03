6a50a604f45f49a0081d2ba7fc2d4ba96b7f176d
Gunnar Sorensen <gunnar.sorensen@slalom.com>
2022-10-03 16:26:29 -0600

    Sort and array of numbers

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('123');
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const NumberCollection_1 = require("./NumberCollection");
+const numberCollection = new NumberCollection_1.NumberCollection([100000, 3, -5, 0]);
+const sorter = new Sorter_1.Sorter(numberCollection);
+sorter.sort();
+console.log(numberCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,7 @@
-console.log('123');
+import { Sorter } from './Sorter';
+import { NumberCollection } from './NumberCollection';
+
+const numberCollection = new NumberCollection([100000, 3, -5, 0]);
+const sorter = new Sorter(numberCollection)
+sorter.sort();
+console.log(numberCollection.data)
