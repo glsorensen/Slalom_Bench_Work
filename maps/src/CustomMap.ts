af1fa59115270d18d359e36175cf9342d97fc285
Gunnar Sorensen <gunnar.sorensen@slalom.com>
2022-10-03 11:50:46 -0600

    Almost done

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,5 +1,11 @@
-import { User } from './User';
-import { Company } from './Company';
+export interface Mappable {
+  location: {
+    lat: number,
+    lng: number,
+  };
+  markerContent(): string;
+  color: string;
+}
 
 export class CustomMap {
 	private googleMap: google.maps.Map;
@@ -14,15 +20,19 @@ export class CustomMap {
 		});
 	}
 
-  addUserMarker(user: User): void {
-    new google.maps.Marker({
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
       }
     });
+    marker.addListener('click', () => {
+      const infoWIndow = new google.maps.InfoWindow({
+				content: mappable.markerContent(),
+      });
+      infoWIndow.open(this.googleMap, marker)
+    });
   }
-
-	addCompanyMarker(company: Company): void {}
 }
