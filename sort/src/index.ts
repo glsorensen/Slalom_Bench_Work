c48d64bb3b74b98e6824ea1de712c6bcc969563c
Gunnar Sorensen <gunnar.sorensen@slalom.com>
2022-10-04 10:18:27 -0600

    Linked List Sort

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumberCollection';
 import { CharacterCollection } from './CharacterCollection';
+import { LinkedList } from './LinkedList';
 
 const numberCollection = new NumberCollection([100000, 3, -5, 0]);
 const numberSorter = new Sorter(numberCollection)
@@ -11,3 +12,14 @@ const characterCollection = new CharacterCollection('weihosdvisefgib');
 const characterSorter = new Sorter(characterCollection);
 characterSorter.sort();
 console.log(characterCollection.data)
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(5);
+linkedList.add(89);
+linkedList.add(-3);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
