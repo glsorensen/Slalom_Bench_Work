fdd35d252f50783be7e5251cfff9f70ec9be19f3
Gunnar Sorensen <gunnar.sorensen@slalom.com>
2022-10-04 15:23:35 -0600

    Update CSV reader to parse data into desired objects

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,4 +1,7 @@
 import fs from 'fs';
+import { parse } from 'path';
+import { dateStringToDate } from './utils';
+import { MatchResult } from './matchResult';
 
 export class CsvFileReader {
   data: string[][] = [];
@@ -7,12 +10,26 @@ export class CsvFileReader {
   
   read(): void {
     this.data = fs
-			.readFileSync(this.filename, {
-				encoding: 'utf-8',
-			})
-			.split('\n')
-			.map((row: string): string[] => {
-				return row.split(',');
-			});
+      .readFileSync(this.filename, {
+        encoding: 'utf-8',
+      })
+      .split('\n')
+      .map(
+        (row: string): string[] => {
+          return row.split(',');
+        }
+      )
+      .map((row: string[]): any => {
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6]
+        ];
+      }
+    );
   }
 }
