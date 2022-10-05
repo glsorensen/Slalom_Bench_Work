62104c32133a20e8e23e7cd5ce4bf43859570291
Gunnar Sorensen <gunnar.sorensen@slalom.com>
2022-10-05 11:04:01 -0600

    Abstract Class for Match Reader

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,35 +1,24 @@
 import fs from 'fs';
-import { parse } from 'path';
-import { dateStringToDate } from './utils';
 import { MatchResult } from './matchResult';
 
-export class CsvFileReader {
-  data: string[][] = [];
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-  constructor(public filename: string) { }
-  
-  read(): void {
-    this.data = fs
-      .readFileSync(this.filename, {
-        encoding: 'utf-8',
-      })
-      .split('\n')
-      .map(
-        (row: string): string[] => {
-          return row.split(',');
-        }
-      )
-      .map((row: string[]): any => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult,
-          row[6]
-        ];
-      }
-    );
-  }
+export abstract class CsvFileReader {
+	data: MatchData[] = [];
+
+	constructor(public filename: string) {}
+
+	abstract mapRow(row: string[]): MatchData;
+
+	read(): void {
+		this.data = fs
+			.readFileSync(this.filename, {
+				encoding: 'utf-8',
+			})
+			.split('\n')
+			.map((row: string): string[] => {
+				return row.split(',');
+			})
+			.map(this.mapRow);
+	}
 }
