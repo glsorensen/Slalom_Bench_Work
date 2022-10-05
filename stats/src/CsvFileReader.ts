015e22d1932776aa8fb34ae77d65ac4c5a3102c6
Gunnar Sorensen <gunnar.sorensen@slalom.com>
2022-10-05 13:36:11 -0600

    Delete this one if breaks

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,21 +1,22 @@
 import fs from 'fs';
 
-export abstract class CsvFileReader<T> {
-	data: T[] = [];
 
-	constructor(public filename: string) {}
 
-	abstract mapRow(row: string[]): T;
+export class CsvFileReader {
+  data: string[][] = [];
 
-	read(): void {
-		this.data = fs
-			.readFileSync(this.filename, {
-				encoding: 'utf-8',
-			})
-			.split('\n')
-			.map((row: string): string[] => {
-				return row.split(',');
-			})
-			.map(this.mapRow);
-	}
+  constructor(public filename: string) { }
+  
+  read(): void {
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: 'utf-8',
+      })
+      .split('\n')
+      .map(
+        (row: string): string[] => {
+          return row.split(',');
+        }
+      );  
+  }
 }
